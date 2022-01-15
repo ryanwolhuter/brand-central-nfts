1dc59e435233a8e4dddb785461fb96965ad7d02c
ryanwolhuter <ryanw@linumlabs.com>
2022-01-15 19:04:19 +0200

    add connect/disconnect button styles

--- components/ConnectWallet.tsx
+++ components/ConnectWallet.tsx
@@ -13,7 +13,7 @@ export default function ConnectWallet() {
     height: ${connectData.connected ? 'auto' : '100%'};
     top: 0;
     left: 0;
-    background: hsla(0, 0%, 100%, 0.5);
+    background: rgba(20, 19, 19, 0.137);
     backdrop-filter: blur(24px);
     z-index: 2;
     display: flex;
@@ -21,20 +21,58 @@ export default function ConnectWallet() {
     justify-content: center;
   `
 
+  const ConnectButton = styled.button`
+    background: linear-gradient(
+      300deg,
+      rgba(46, 214, 226, 0.315) -2.88%,
+      rgba(94, 75, 196, 0.54) 60.96%
+    );
+    color: white;
+    font-size: 18px;
+    cursor: pointer;
+    font-weight: 500;
+    outline: none;
+    border: none;
+    border-radius: 30px;
+    padding: 16px 24px;
+    position: relative;
+    &::before {
+      content: '';
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      border-radius: 30px;
+      padding: 1px;
+      background: linear-gradient(
+        45deg,
+        hsla(0, 0%, 100%, 0.2),
+        hsla(0, 0%, 100%, 0)
+      );
+      -webkit-mask: linear-gradient(#fff 0 0) content-box,
+        linear-gradient(#fff 0 0);
+      -webkit-mask-composite: xor;
+      mask-composite: exclude;
+    }
+    box-shadow: 0px 0px 30px 2px rgba(91, 3, 233, 0.24);
+  `
+
+  const DisconnectButton = styled(ConnectButton)`
+    margin-left: auto;
+    margin-right: 32px;
+    margin-block: 16px;
+    padding: 8px 24px;
+  `
+
   return (
     <Wrapper>
       {accountData ? (
-        <div>
-          {accountData.address}
-          <button onClick={disconnect}>Disconnect</button>
-        </div>
+        <DisconnectButton onClick={disconnect}>Disconnect</DisconnectButton>
       ) : (
-        <button
-          disabled={!metamask.ready}
-          onClick={() => connect(metamask)}
-        >
+        <ConnectButton disabled={!metamask.ready} onClick={() => connect(metamask)}>
           Unlock with MetaMask
-        </button>
+        </ConnectButton>
       )}
     </Wrapper>
   )
